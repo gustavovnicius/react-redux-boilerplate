6c9b3ac87b783d2abd9515896d6799d1d2a49e08
Gustavo Vinícius <gustavo@webgoal.com.br>
2016-05-04 14:23:13 -0300

    Fixing the karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -20,6 +20,12 @@ module.exports = function(config) {
         },{
           test: /\.scss$/,
           loader: 'style!css!sass'
+        },{
+          test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          loader: "url-loader?limit=10000&minetype=application/font-woff"
+        },{
+          test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          loader: "file-loader"
         }]
       },
       watch: true,
