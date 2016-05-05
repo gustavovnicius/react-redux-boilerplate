63d1f1dcf188b6781e84ad659cd995b632836c08
Gustavo Vinícius <gustavovnicius@hotmail.com>
2016-05-04 22:21:20 -0300

    Fixing the webpack production config

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -17,6 +17,12 @@ var config = {
     },{
       test: /\.scss$/,
       loader: 'style!css!sass'
+    },{
+      test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      loader: "url-loader?limit=10000&minetype=application/font-woff"
+    },{
+      test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      loader: "file-loader"
     }]
   },
   plugins: [
