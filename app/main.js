bf1563aac7c5a98a071be2a567e1a6c81d766f04
Gustavo Vinícius <gustavovnicius@hotmail.com>
2016-05-13 00:34:08 -0300

    Applying logger middleware

--- app/main.js
+++ app/main.js
@@ -3,12 +3,13 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, compose, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import createLogger from 'redux-logger'
 import reducers from './reducers/index'
 import AppPage from './containers/AppPage'
 
 import { Router, Route, hashHistory } from 'react-router'
 
-let store = createStore(reducers, compose(applyMiddleware(thunk), window.devToolsExtension ? window.devToolsExtension() : f => f))
+let store = createStore(reducers, compose(applyMiddleware(thunk, createLogger()), window.devToolsExtension ? window.devToolsExtension() : f => f))
 
 render(
   <Provider store={store}>
